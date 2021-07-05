4eef4f4b8389f8908f5a4ccd04996f09dac869ae
NaMooJoon <47983023+NaMooJoon@users.noreply.github.com>
2021-07-05 21:30:01 +0900

    Practicing JavaScript syntax

--- syntax/callback.js
+++ syntax/callback.js
@@ -3,8 +3,14 @@ function a(){
     console.log('A');
 }
 */
+// JavaScript에서는 함수가 값이다.
 var a = function(){
     console.log('A');
 }
 
-a();
+
+function slowfunc(callback){
+    callback();
+}
+
+slowfunc(a);
